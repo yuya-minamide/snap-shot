31bf261a0e70bff32f2be7a2dc15b93795220654
Yuya Minamide <dettu1003@gmail.com>
2023-07-28 20:26:28 -0700

    added download image feature

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { AiOutlineDownload } from "react-icons/ai";
 
 interface ImageProps {
 	url: string;
 	title: string;
 }
 
-const Image: React.FC<ImageProps> = ({ url, title }) => (
-	<li>
-		<img src={url} alt={title} />
-	</li>
-);
+const Image: React.FC<ImageProps> = ({ url, title }) => {
+	const handleDownload = async () => {
+		try {
+			const response = await fetch(url);
+			const blob = await response.blob();
+			const objectURL = URL.createObjectURL(blob);
+
+			// Put temporary anchor element to trigger the download
+			const link = document.createElement("a");
+			link.href = objectURL;
+			link.download = title;
+			document.body.appendChild(link);
+			link.click();
+
+			// Clean up the temporary anchor
+			document.body.removeChild(link);
+			URL.revokeObjectURL(objectURL);
+		} catch (error) {
+			console.error("Error downloading image:", error);
+		}
+	};
+
+	return (
+		<li className="img-container">
+			<img src={url} alt={title} />
+			<div className="download-icon" onClick={handleDownload}>
+				<AiOutlineDownload />
+			</div>
+		</li>
+	);
+};
 
 export default Image;
